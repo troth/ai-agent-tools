9d9fd26ba85d9926f1bb0fd1a8df562ae04a2f78
Theodore A. Roth <troth@openavr.org>
2026-05-24 17:33:34 -0600

    compose: Rework image naming
    Eliminate the need to define image name in env file.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- setup-docker-env.sh
+++ setup-docker-env.sh
@@ -3,7 +3,7 @@
 cat <<EOF >.env
 # This file was generated using ./setup-docker-env.sh.
 # The following environment variables are used by 'docker compose'.
-MY_IMAGE_NAME="openavr/agent-tools-troth"
+
 MY_USERNAME=${USER}
 MY_UID=$(id -u)
 MY_GID=$(id -g)
